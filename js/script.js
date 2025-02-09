7b6ceb7742baea63012327d1570c135b0099e3c3
egirizaldi26 <ragilia889@gmail.com>
2025-02-09 12:25:52 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika Hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+
+
+// Diluar sidebar  untuk menghilangkan nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove("active");
+    }
+});
